f9899c5c7a82183aeebde3b0e083068ee179f137
nicolebarleta <mnbarleta@my365.bellevue.edu>
2020-08-05 12:09:27 -0500

    commit

---
+++ week-1/barleta-exercise-1.3.js
@@ -0,0 +1,30 @@
+/*
+============================================
+; Title:  barleta-exercise-1.3.js
+; Author: Professor Krasso
+; Date:   5 Aug 2020
+; Modified By: Marie Nicole Barleta
+; Description:
+;===========================================
+*/
+
+// import statement
+const header = require('../barleta-header.js');
+
+console.log(header.display("Marie", "Barleta", "Exercise 1.3"));
+
+/*
+ Expected output:
+ FirstName LastName
+ <AssignmentName>
+ <Today's Date>
+ -- DISPLAYING CELL PHONE DETAILS --
+ Manufacturer: <manufacturer>
+ Model: <model>
+ Color: <color>
+ Price: <price>
+*/
+
+// start program
+
+// end program
---
+++ week-1/barleta-exercise-1.4.js
@@ -0,0 +1,14 @@
+/*
+============================================
+; Title:  barleta-exercise-1.4.js
+; Author: Professor Krasso
+; Date:   5 Aug 2020
+; Modified By: Marie Nicole Barleta
+; Description:
+;===========================================
+*/
+
+// import statement
+const header = require('../barleta-header.js');
+
+console.log(header.display("Marie", "Barleta", "Exercise 1.4"));
